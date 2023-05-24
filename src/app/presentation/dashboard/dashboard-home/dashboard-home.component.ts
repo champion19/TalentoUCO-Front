20966dac1aca48db4b4d855272706592c2029d2c
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-05-24 16:37:12 -0500

    dashboard components

---
+++ src/app/presentation/dashboard/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: ['./dashboard-home.component.css']
+})
+export class DashboardHomeComponent {
+
+}
