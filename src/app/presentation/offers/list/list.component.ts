6af273d18d3bf6851e244e81db518f5921ac9336
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-05-26 15:21:59 -0500

    mejoras en list.component

--- src/app/presentation/offers/list/list.component.ts
+++ src/app/presentation/offers/list/list.component.ts
@@ -11,46 +11,46 @@ listOffers=[
   id:1,
   title:"Software Developer",
   status:'Activa',
-  date_status:"Enviada a revision:2023-05-11 - 11:45",
+  date_status:"Enviada a revision:2023-05-24 - 11:45",
   location:"Medellin y sus alrededores",
-  money:"De $1 a $5 millones",
+  money:"De $2 a $5 millones",
   modality:"Remoto"
 },
 {
       id: 2,
       title: "Database Administrator",
       status: 'En Revision',
-      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      date_status: 'Enviada a revisión: 2023-05-25 - 11:45',
       location: "Medellín y sus alrededores",
       money: "De $1 a $5 millones",
       modality: "Remoto"
 },
 {
   id: 3,
-      title: "Database Administrator",
-      status: 'En Revision',
-      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      title: "FullStack Developer",
+      status: 'Disponible',
+      date_status: 'Enviada a revisión: 2023-05-26 - 11:45',
       location: "Medellín y sus alrededores",
-      money: "De $1 a $5 millones",
+      money: "De $1 a $6 millones",
       modality: "Remoto"
 
 },
 {
 id: 4,
-      title: "Database Administrator",
-      status: 'En Revision',
-      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      title: "Backend Developer",
+      status: 'Disponible',
+      date_status: 'Enviada a revisión: 2023-05-23 - 11:45',
       location: "Medellín y sus alrededores",
-      money: "De $1 a $5 millones",
+      money: "De $1 a $4 millones",
       modality: "Remoto"
 },
 {
  id: 5,
-      title: "Database Administrator",
-      status: 'En Revision',
-      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
-      location: "Medellín y sus alrededores",
-      money: "De $1 a $5 millones",
+      title: "Frontend Developer",
+      status: 'Disponible',
+      date_status: 'Enviada a revisión: 2023-05-22 - 11:45',
+      location: "Medellín",
+      money: "De $1 a $6 millones",
       modality: "Remoto"
 },
 ];
