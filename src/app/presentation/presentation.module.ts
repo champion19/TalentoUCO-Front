5bbae20a7741d2ca95db3c2f5b4c45f501cdb392
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-06-01 10:08:02 -0500

    mejoras y nuevas imagenes

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule
